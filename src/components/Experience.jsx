62ca78beb05282f65c25980a60a430985a72dba3
Haram Iqbal Nagra <hnagra.bscs21seecs@seecs.edu.pk>
2024-08-19 22:05:50 +0500

    hoc done

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,10 +1,10 @@
-import {VerticalTimeline,VerticalTimelineElement} from 'react-vertical-timeline-component';
-import {motion} from 'framer-motion';
+import { motion } from 'framer-motion';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 
-import {styles} from '../styles'
-import {experiences} from '../constants'
-import {SectionWrapper} from '../hoc'
+import { experiences } from '../constants';
+import { SectionWrapper } from '../hoc';
+import { styles } from '../styles';
 import { textVariant } from '../utils/motion';
 
 const ExperienceCard=({experience})=>(
@@ -27,18 +27,21 @@ const ExperienceCard=({experience})=>(
     <h3 className='text-white text-[24px] font-bold '>
       {experience.title}
     </h3>
-    <p className='text-secondary text-[16px] font-semibold'>
+    <p className='text-secondary text-[16px] font-semibold' style={{margin:0}}>
       {experience.company_name}
     </p>
+    <ul className='mt-5 list-disc ml-5 space-y-2'>
+      {experience.points.map((point,index)=>(
+        <li
+          key={`experience-point-$(index)`}
+          className='text-white-100 text-[14px] pl-1 tracking-wider'>
+            {point}
+        </li>
+      ))}
+    </ul>
   </div>
   </VerticalTimelineElement>
 )
-
-
-
-
-
-
 
 const Experience = () => {
   return (
